449481758f2fb9d31faab0d05d64b88a126b66c6
abhaya-ak <abhayakunwar502@gmail.com>
2026-05-14 22:40:26 +0545

    i deleted authentication module, while testing bug was found and i am recreating it

--- config/urls.py
+++ config/urls.py
@@ -20,10 +20,7 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view()),
-    path('api/token/refresh/', TokenRefreshView.as_view()),
     path('api/student/', include('students.urls')),
     path('api/fees/', include('fees.urls')),
     path('admin/', admin.site.urls),
-    path('api/v1/auth/', include('authentication.urls')),
 ]
